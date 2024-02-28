49035a3e09309f0f3badf97ca4ca68e44fa91bbd
Bin Zhang <zncoder@gmail.com>
2024-02-28 00:17:13 -0800

    rsync

--- watchedrsync.go
+++ watchedrsync.go
@@ -2,27 +2,37 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"path/filepath"
+	"strings"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/zncoder/check"
+	"github.com/zncoder/mygo"
+)
+
+var (
+	baseDir    string
+	remotePath string
 )
 
 func main() {
-	remoteDir := flag.String("r", "", "remote dir in rsync format, e.g. foo:bar/")
+	flag.StringVar(&remotePath, "r", "", "remote path in rsync format, e.g. foo:bar")
 	flag.Parse()
-	check.T(*remoteDir != "").F("no remotedir")
-	check.T(flag.NArg() > 0).F("no dir")
-	check.L("sync to remote", "dir", *remoteDir)
+	check.T(remotePath != "").F("no remotedir")
+	check.T(flag.NArg() == 1).F("no dir")
+	check.L("sync to remote", "dir", remotePath)
+	remotePath = filepath.Clean(remotePath)
 
 	watcher := check.V(fsnotify.NewWatcher()).F("NewWatcher")
+
 	go watchLoop(watcher)
 
-	for _, dir := range flag.Args() {
-		dir = check.V(filepath.Abs(dir)).F("invalid dir", "dir", dir)
-		check.E(watcher.Add(dir))
-		check.L("watching", "dir", dir)
-	}
+	baseDir = flag.Arg(0)
+	baseDir = check.V(filepath.Abs(baseDir)).F("invalid dir", "dir", baseDir)
+	check.E(watcher.Add(baseDir))
+	check.L("watching", "dir", baseDir)
+
 	select {}
 }
 
@@ -31,7 +41,19 @@ func watchLoop(watcher *fsnotify.Watcher) {
 		select {
 		case ev, ok := <-watcher.Events:
 			check.T(ok).F("recv watcher event")
-			check.L("received", "event", ev)
+			// TODO: process async
+			// TODO: handle remove, rename by rsync the whole dir
+			// TODO: handle create of dir
+			if ev.Op == fsnotify.Create || ev.Op == fsnotify.Write {
+				processEvent(ev.Name)
+			}
 		}
 	}
+}
+
+func processEvent(filename string) {
+	p := strings.TrimPrefix(filename, baseDir)
+	dest := fmt.Sprintf("%s/%s", remotePath, p)
+	check.L("rsync", "filename", filename, "dest", dest)
+	mygo.NewCmd("rsync", "-a", filename, dest).Run()
 }
